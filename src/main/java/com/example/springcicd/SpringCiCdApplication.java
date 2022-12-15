68186f56dd00f7338165f6378053093c892710db
Yuriy Titov <yurtitov@gmail.com>
2022-12-15 23:49:05 +0200

    Initial commit

---
+++ src/main/java/com/example/springcicd/SpringCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCiCdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcicd/MainControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.springcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MainControllerTest {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
